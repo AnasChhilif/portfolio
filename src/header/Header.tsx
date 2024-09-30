855041563348ff93425dbacf56db22568108bf14
Anas Chhilif <anaschhilif33@gmail.com>
2024-09-30 07:56:27 +0000

    fixing header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -10,17 +10,16 @@ const Navbar = () => {
 
     const navItems = [
         { id: 1, text: 'Home' },
-        { id: 2, text: 'Company' },
-        { id: 3, text: 'Resources' },
-        { id: 4, text: 'About' },
+        { id: 2, text: 'About' },
+        { id: 3, text: 'Tech' },
+        { id: 4, text: 'Work' },
         { id: 5, text: 'Contact' },
     ];
 
     return (
         <div className='flex justify-between items-center w-full p-5' >
-            <h1 className='w-3/5 text-3xl font-bold '>Anas</h1>
 
-            <ul className='hidden md:w-2/5 md:flex md:justify-between '>
+            <ul className='hidden md:w-1/3 md:flex md:justify-between '>
                 {navItems.map(item => (
                     <li
                         key={item.id}
@@ -40,23 +39,25 @@ const Navbar = () => {
             <ul
                 className={
                     nav
-                        ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+                        ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 ease-in-out duration-500 mt-10'
                         : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
                 }
             >
-                {/* Mobile Logo */}
-                <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
 
                 {/* Mobile Navigation Items */}
                 {navItems.map(item => (
                     <li
                         key={item.id}
-                        className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+                        className='p-4 border-b rounded-xl duration-300 cursor-pointer border-gray-600'
                     >
                         {item.text}
                     </li>
                 ))}
             </ul>
+
+            <h1 className='w-1/3 text-3xl font-bold text-center'>Anas</h1>
+
+            <button className='w-1/3 text-end'>light</button>
         </div>
     );
 };
